0c527dd34cabd550cc104c030ad9151df67abd18
CelulaBandaAncha <72107627+CelulaBandaAncha@users.noreply.github.com>
2022-11-14 22:25:20 -0500

    commit origin

--- laboratorio-springboot-java-postgres/src/main/java/com/ruiz/spring/jpa/postgresql/controller/PruebaController.java
+++ laboratorio-springboot-java-postgres/src/main/java/com/ruiz/spring/jpa/postgresql/controller/PruebaController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ruiz.spring.jpa.postgresql.repository.PruebaRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+//@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class PruebaController {
